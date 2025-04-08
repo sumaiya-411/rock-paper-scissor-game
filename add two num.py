ce21a31de4c5203f5b596d1b536a49ba4925228d
Sumaiya Kouser <misssumaiya411@gmail.com>
2025-04-08 14:33:47 +0530

    initial commit

---
+++ add two num.py
@@ -0,0 +1,11 @@
+# Basic Python Program: Add Two Numbers
+
+# Ask the user to enter two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", sum)
